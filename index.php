81277f294dd61a0c62086d62e4ae9c9722ee555a
training <training@STUDENT.IBAT>
2019-04-24 20:18:48 +0100

    somestuff

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php 
 echo "hello world this is version 2";
 echo "<P> PASSWORD IS " . getenv('DB_PASSWORD');
+
+class DBikes{
+    public $number;
+    public $name;
+}
+
+$listOfBikes = []
+
+for ($i = 0; $i< 10; $i++){
+    $bike = new DBike;
+    $bike -> number = "$i";
+    $bike -> name="Name $i";
+    $listOfBikes[] = $bike;
+}
+
+/* print_r($listOfBikes); */
+header('Content-Type: application/json');
+
+$jsonOutput = json_encode($listOfBikes);
+
+echo $jsonOutput
